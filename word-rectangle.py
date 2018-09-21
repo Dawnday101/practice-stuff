5b929d46ee19c6befb977e8f797f8c3874b6808a
Tamara <dawnday101@gmail.com>
2018-09-21 10:15:32 -0400

    Update word-rectangle.py

--- word-rectangle.py
+++ word-rectangle.py
@@ -1,11 +1,11 @@
-def rect(word="SHITPOST", width=5, height=5):
-    let_order = word + ''.join(word[-2:0:-1])
-    for y in range(0, (len(word) - 1) * height + 1):
-        for x in range(0, (len(word) - 1) * width + 1):
-            if x % (len(word) - 1) == 0 or y % (len(word) - 1) == 0:
+def rect(phrase="Thank You Justin!", width=5, height=5):
+    let_order = phrase + ''.join(phrase[-2:0:-1])
+    for y in range(0, (len(phrase) - 1) * height + 1):
+        for x in range(0, (len(phrase) - 1) * width + 1):
+            if x % (len(phrase) - 1) == 0 or y % (len(phrase) - 1) == 0:
                 print(let_order[(x + y) % len(let_order)], end="")
             else:
                 print(' ', end="")
         print()
     
-rect("SHITPOST", 6, 3)
+rect("Thank You Justin!", 6, 3)
